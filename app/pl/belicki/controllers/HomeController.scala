67b6ddbfa3b450868b7645f3045a08fc79915542
Marcin Belicki <marcin.belicki@protonmail.com>
2025-09-07 19:09:25 +0200

    removing unnecessary stuff

--- app/pl/belicki/controllers/HomeController.scala
+++ app/pl/belicki/controllers/HomeController.scala
@@ -7,23 +7,14 @@ import play.api.mvc._
 import play.json.mapper.JsonMapperExtensions
 
 import javax.inject._
+import scala.concurrent.Future
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(val jsonMapper: JsonMapper with ClassTagExtensions, val controllerComponents: ControllerComponents) extends BaseController with JsonMapperExtensions {
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action(parse.jacksonJson[Message]) { implicit request: Request[Message] =>
-    println(request.body)
+  def index(): Action[Message] = Action.async(parse.jacksonJson[Message]) {
+    implicit request: Request[Message] =>
+      println(request.body)
 
-    Ok(request.body.message(4).toString)
+      Future.successful(Ok(request.body.message(4).toString))
   }
 }
